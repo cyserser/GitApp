c429fc8b6a854005d587f177d6d42d75ba5d57ee
thunder <cyserser@gmail.com>
2020-02-14 10:56:41 +0000

    T01: initial screen ( para oneline !!!)
    Adding documentation to my initial screen :^)

--- app/src/main/java/com/example/gitapp/MainActivity.java
+++ app/src/main/java/com/example/gitapp/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Documentation incial de onCreate
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
